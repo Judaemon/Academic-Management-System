abdcf1bf3d07c5c477a1cb4f761673a9b90b9de9
mjklench <klench.mjkl@gmail.com>
2022-10-01 12:53:27 +0800

    teacher-sections

---
+++ database/migrations/2022_09_30_162029_create_sections_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('sections', function (Blueprint $table) {
+            $table->id();
+
+            $table->string('name');
+            $table->string('subjects_id');
+            $table->string('class_limit');
+            $table->string('column_5');
+            $table->string('grade_level_id');
+
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('sections');
+    }
+};
