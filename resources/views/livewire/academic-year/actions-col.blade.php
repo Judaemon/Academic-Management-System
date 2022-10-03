93be18186142c0e47a4df848d4a37a1fddca8665
pR3ssU53r3 <90381966+pR3ssU53r3@users.noreply.github.com>
2022-10-03 20:35:48 +0800

    ....

---
+++ resources/views/livewire/academic-year/actions-col.blade.php
@@ -0,0 +1,4 @@
+<div>
+    <x-button icon="pencil" info onclick="$openModal('modalEdit')" label="Edit" />
+    <x-button icon="trash" negative onclick="#"  label="Remove" />
+</div>
---
+++ resources/views/livewire/academic-year/create-academic-year.blade.php
@@ -0,0 +1,26 @@
+<div>
+    <x-button primary onclick="$openModal('modalCreate')" label="ADD ACADEMIC YEAR LEVEL" />
+    
+    <x-modal wire:model.defer="modalCreate" max-width="2xl">
+        <x-card title="CREATE ACADEMIC YEAR">
+            <div class="grid grid-cols-1 gap-4">
+                <div class="col-span-4">
+                    <x-input wire:model.defer="academic_year.year" label="Academic Year Level" placeholder="" />
+                </div>
+  
+                <div class="col-span-4">
+                    <x-textarea wire:model="academic_year.curriculum" label="Curriculum" placeholder="Enter curriculum description..." />
+                </div>
+  
+            </div>
+  
+            <x-slot name="footer">
+                <div class="flex justify-end gap-x-4">
+                    <x-button flat label="Cancel" wire:click="closeModal" />
+  
+                    <x-button wire:click="save" type="button" primary label="Save" />
+                </div>
+            </x-slot>
+        </x-card>
+    </x-modal>
+  </div>
