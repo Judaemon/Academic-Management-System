4c7190e94aa85f9de9bfa4affdf27ac140a2ef68
pR3ssU53r3 <90381966+pR3ssU53r3@users.noreply.github.com>
2022-10-03 18:57:43 +0800

    ...

---
+++ database/migrations/2022_09_27_010624_create_academic_years_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('academic_years', function (Blueprint $table) {
+            $table->id();
+            $table->year('year');
+            $table->text('curriculum');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('academic_years');
+    }
+};
