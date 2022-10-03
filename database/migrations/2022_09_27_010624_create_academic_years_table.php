302f3308e7a27e40ba2c4e9f6bd2259934723d1a
pR3ssU53r3 <90381966+pR3ssU53r3@users.noreply.github.com>
2022-10-03 20:40:27 +0800

    ....

--- database/migrations/2022_09_27_010624_create_academic_years_table.php
+++ database/migrations/2022_09_27_010624_create_academic_years_table.php
@@ -15,7 +15,7 @@ return new class extends Migration
     {
         Schema::create('academic_years', function (Blueprint $table) {
             $table->id();
-            $table->year('year');
+            $table->string('year');
             $table->text('curriculum');
             $table->timestamps();
         });
