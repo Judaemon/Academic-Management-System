605ccb63a1fc1f1383d0202a419079f307aa031b
John Ray Judaya <judaya93@gmail.com>
2022-10-04 21:49:51 +0800

    admin links moved to dropdown

--- resources/views/layouts/navigation.blade.php
+++ resources/views/layouts/navigation.blade.php
@@ -74,18 +74,23 @@
             </li>
 
             <li class="relative px-6 py-3">
-                <button class="inline-flex items-center justify-between w-full text-sm font-semibold transition-colors duration-150 hover:text-gray-800 dark:hover:text-gray-200"
+                @if (request()->routeIs('roles.index') || request()->routeIs('setting.index'))
+                    <div x-init="toggleMultiLevelMenu"></div>
+                @endif
+
+                <button class="inline-flex items-center justify-between w-full text-sm font-semibold transition-colors duration-150 text-gray-800 dark:hover:text-gray-200"
                         @click="toggleMultiLevelMenu" aria-haspopup="true">
-                <span class="inline-flex items-center">
-                    <svg class="w-5 h-5" aria-hidden="true" fill="none" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" viewBox="0 0 24 24" stroke="currentColor">
-                        <path d="M4 6h16M4 10h16M4 14h16M4 18h16"></path>
-                    </svg>
-                    <span class="ml-4">Two-level menu</span>
-                </span>
+                    <span class="inline-flex items-center">
+                        <svg class="w-5 h-5" aria-hidden="true" fill="none" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" viewBox="0 0 24 24" stroke="currentColor">
+                            <path d="M4 6h16M4 10h16M4 14h16M4 18h16"></path>
+                        </svg>
+                        <span class="ml-4">Administrator</span>
+                    </span>
                     <svg class="w-4 h-4" aria-hidden="true" fill="currentColor" viewBox="0 0 20 20">
                         <path fill-rule="evenodd"
                               d="M5.293 7.293a1 1 0 011.414 0L10 10.586l3.293-3.293a1 1 0 111.414 1.414l-4 4a1 1 0 01-1.414 0l-4-4a1 1 0 010-1.414z"
-                              clip-rule="evenodd"></path>
+                              clip-rule="evenodd">
+                        </path>
                     </svg>
                 </button>
                 <template x-if="isMultiLevelMenuOpen">
@@ -95,14 +100,39 @@
                         x-transition:leave-start="opacity-100 max-h-xl" x-transition:leave-end="opacity-0 max-h-0"
                         class="p-2 mt-2 space-y-2 overflow-hidden text-sm font-medium text-gray-500 rounded-md shadow-inner bg-gray-50 dark:text-gray-400 dark:bg-gray-900"
                         aria-label="submenu">
-                        <li class="px-2 py-1 transition-colors duration-150 hover:text-gray-800 dark:hover:text-gray-200">
-                            <a class="w-full" href="#">Child menu</a>
+
+                        @can('view_roles')
+                        <li class="relative px-6 py-3">
+                            <x-nav-link href="{{ route('roles.index') }}" :active="request()->routeIs('roles.index')">
+                                <x-slot name="icon">
+                                    <svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor" stroke-width="2">
+                                        <path stroke-linecap="round" stroke-linejoin="round" d="M12 6V4m0 2a2 2 0 100 4m0-4a2 2 0 110 4m-6 8a2 2 0 100-4m0 4a2 2 0 110-4m0 4v2m0-6V4m6 6v10m6-2a2 2 0 100-4m0 4a2 2 0 110-4m0 4v2m0-6V4" />
+                                    </svg>
+                                </x-slot>
+                                {{ __('Roles and Permission') }}
+                            </x-nav-link>
                         </li>
+                        @endcan
+
+                        @can('view_system')
+                        <li class="relative px-6 py-3">
+                            <x-nav-link href="{{ route('setting.index') }}" :active="request()->routeIs('setting.index')">
+                                <x-slot name="icon">
+                                    <svg class="h-5 w-5" aria-hidden="true" fill="none" viewBox="0 0 24 24" stroke="currentColor" stroke-width="2">
+                                        <path stroke-linecap="round" stroke-linejoin="round" d="M10.325 4.317c.426-1.756 2.924-1.756 3.35 0a1.724 1.724 0 002.573 1.066c1.543-.94 3.31.826 2.37 2.37a1.724 1.724 0 001.065 2.572c1.756.426 1.756 2.924 0 3.35a1.724 1.724 0 00-1.066 2.573c.94 1.543-.826 3.31-2.37 2.37a1.724 1.724 0 00-2.572 1.065c-.426 1.756-2.924 1.756-3.35 0a1.724 1.724 0 00-2.573-1.066c-1.543.94-3.31-.826-2.37-2.37a1.724 1.724 0 00-1.065-2.572c-1.756-.426-1.756-2.924 0-3.35a1.724 1.724 0 001.066-2.573c-.94-1.543.826-3.31 2.37-2.37.996.608 2.296.07 2.572-1.065z" />
+                                        <path stroke-linecap="round" stroke-linejoin="round" d="M15 12a3 3 0 11-6 0 3 3 0 016 0z" />
+                                    </svg>
+                                </x-slot>
+                                {{ __('System Setting') }}
+                            </x-nav-link>
+                        </li>
+                        @endcan
+
                     </ul>
                 </template>
             </li>
 
-            @can('view_roles')
+            {{-- @can('view_roles')
             <li class="relative px-6 py-3">
                 <x-nav-link href="{{ route('roles.index') }}" :active="request()->routeIs('roles.index')">
                     <x-slot name="icon">
@@ -113,9 +143,9 @@
                     {{ __('Roles and Permission') }}
                 </x-nav-link>
             </li>
-            @endcan
+            @endcan --}}
 
-            @can('view_system')
+            {{-- @can('view_system')
             <li class="relative px-6 py-3">
                 <x-nav-link href="{{ route('setting.index') }}" :active="request()->routeIs('setting.index')">
                     <x-slot name="icon">
@@ -127,7 +157,7 @@
                     {{ __('System Setting') }}
                 </x-nav-link>
             </li>
-            @endcan
+            @endcan --}}
 
             {{-- student --}}
             {{-- student --}}
