ad06047baf37b1e4bd68e636c73ea992fe290fa9
pR3ssU53r3 <90381966+pR3ssU53r3@users.noreply.github.com>
2022-10-06 16:47:34 +0800

    remove payment editing

--- resources/views/livewire/payment/actions-col.blade.php
+++ resources/views/livewire/payment/actions-col.blade.php
@@ -1,12 +1,5 @@
 <div>
-    @can('edit_payment')
-        <x-button 
-          icon="pencil" 
-          info 
-          onclick="livewire.emit('openModal', 'payments.edit-payment', {{ json_encode(['payment' => $value]) }})" 
-          label="Edit" 
-        />    
-    @elsecan('delete_payment')
+    @can('delete_payment')
         <x-button 
           icon="trash" 
           negative 
