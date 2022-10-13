763fbacd59b587d2b8a3d0076be17e4d3e72bdde
John Ray Judaya <judaya93@gmail.com>
2022-10-13 14:30:11 +0800

    added user API

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -3,6 +3,9 @@
 namespace App\Http\Controllers;
 
 use App\Models\User;
+use Illuminate\Contracts\Database\Eloquent\Builder;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
 
 class UserController extends Controller
 {
@@ -13,5 +16,27 @@ class UserController extends Controller
         }
 
         return view('users.index');
+    }
+
+    public function teachers(Request $request)
+    {
+        // checks if there already a selected item
+        if ($request->has('selected')) {
+            return User::query()
+                ->role('Teacher')
+                ->select(DB::raw("CONCAT(firstname, ' ', lastname) AS full_name"),'id')
+                ->where('id', $request->selected)
+                ->get();
+        }
+    
+        return User::query()
+        ->role('Teacher')
+        ->select(DB::raw("CONCAT(firstname, ' ', lastname) AS full_name"),'id')
+        ->when(
+            $request->search,
+            fn (Builder $query) => $query
+                ->where(DB::raw('CONCAT_WS(" ", firstname, lastname)'), 'like', "%{$request->search}%")
+        )
+        ->get();
     }
 }
