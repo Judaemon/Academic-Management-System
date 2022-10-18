9d3770bc04281c1aff4e3861aaf7f570815a716f
John Ray Judaya <judaya93@gmail.com>
2022-10-18 11:02:30 +0800

    APIs

--- app/Http/Controllers/GradeLevelController.php
+++ app/Http/Controllers/GradeLevelController.php
@@ -5,11 +5,31 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 
 use App\Models\GradeLevel;
+use Illuminate\Contracts\Database\Eloquent\Builder;
+use Illuminate\Support\Facades\DB;
 
 class GradeLevelController extends Controller
 {
     public function index()
     {
         return view('grade-levels.index');
+    }
+
+    public function grade_level(Request $request)
+    {
+        return GradeLevel::query()
+            ->when(
+                $request->search,
+                fn (Builder $query) => $query
+                    ->where('name', 'like', "%{$request->search}%")
+                    ->orWhere('id', 'like', "%{$request->search}%")
+            )
+            ->when(
+                $request->exists('selected'),
+                fn (Builder $query) => $query->whereIn('id', $request->input('selected', [])),
+                fn (Builder $query) => $query->limit(10)
+            )
+            ->orderBy('name')
+            ->get();
     }
 }
--- routes/api.php
+++ routes/api.php
@@ -1,6 +1,8 @@
 <?php
 
+use App\Http\Controllers\GradeLevelController;
 use App\Http\Controllers\RolesController;
+use App\Http\Controllers\SubjectController;
 use App\Http\Controllers\UserController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
@@ -24,3 +26,7 @@ Route::get('teachers', [UserController::class, 'teachers'])->name('users.teacher
 Route::get('users', [UserController::class, 'users'])->name('users.users');
 
 Route::get('roles', [RolesController::class, 'roles'])->name('roles.roles');
+
+Route::get('subjects', [SubjectController::class, 'subjects'])->name('subjects.subjects');
+
+Route::get('grade_level', [GradeLevelController::class, 'grade_level'])->name('grade_level.grade_level');
