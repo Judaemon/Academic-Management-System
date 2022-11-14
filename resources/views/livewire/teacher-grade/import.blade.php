34cf0a8297e5ee5fcdea498509a0c1a28c9f6e44
mjklench <klench.mjkl@gmail.com>
2022-11-14 21:30:06 +0800

    Update import.blade.php

--- resources/views/livewire/teacher-grade/import.blade.php
+++ resources/views/livewire/teacher-grade/import.blade.php
@@ -1,13 +1,13 @@
 <div>
     <form wire:submit.prevent="import" enctype="multipart/form-data">
         @csrf
+        <button class="h-8 px-4 rounded text-indigo-100 transition-colors duration-150 bg-indigo-700 rounded-lg focus:shadow-outline hover:bg-indigo-800">
+            <span class="py-2 px-4 font-semibold">Import CSV</span>
+        </button>
         <input class="px-2 py-2" type="file" wire:model="importFile" class="@error('import_file') is-invalid @enderror">
         @error('import_file')
             <span class="invalid-feedback" role="alert">{{ $message }}</span>
         @enderror
-        <button class="h-8 px-4 rounded text-indigo-100 transition-colors duration-150 bg-indigo-700 rounded-lg focus:shadow-outline hover:bg-indigo-800">
-            <span class="py-2 px-4 font-semibold">Import CSV</span>
-        </button>
     </form>
 
     @if($importing && !$importFinished)
