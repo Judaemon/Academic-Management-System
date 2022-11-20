f9e932cb58e0357802539666ff44f85c456712e0
John Ray Judaya <judaya93@gmail.com>
2022-11-21 02:20:26 +0800

    try payments refund

--- resources/views/livewire/payment/actions-col.blade.php
+++ resources/views/livewire/payment/actions-col.blade.php
@@ -1,13 +1,41 @@
 <div class="flex flex-row space-x-2 justify-center items-center">
     @can('read_payment')
-        <x-button wire:ignore.self icon="eye" green label="View"  
-            onclick="livewire.emit('openModal', 'payments.view-payment', {{ json_encode(['payment' => $row->id]) }})" 
-        />
+        <x-button icon="eye" green label="View"
+            onclick="livewire.emit('openModal', 'payments.view-payment', {{ json_encode(['payment' => $row->id]) }})" />
     @endcan
 
     @can('update_payment')
-        @if($row->payment_status !== 'Refunded')
-            <x-button wire:ignore.self icon="receipt-refund" warning label="Refund" wire:click="refund({{$row->id}})" /> 
+        @if ($row->payment_status !== 'Refunded')
+            <x-button icon="receipt-refund" warning label="Refund" wire:click="refund({{ $row->id }})" />
         @endif
+
+        <x-dialog id="custom" title="User information" description="Complete your profile, give your name">
+            <x-input label="What's your name?" placeholder="your name bro" x-model="name" />
+        </x-dialog>
     @endcan
+
+    {{-- try  --}}
+    <x-button warning icon="receipt-refund"
+        x-on:click="$wireui.confirmDialog({
+                id: 'custom',
+                icon: 'question',
+                accept: {
+                    label: 'Yes, save it',
+                    method: 'update',
+                    params: {{ $row->id }},
+                    execute: () => 'update{{ $row->id }}'
+
+                    {{-- execute: () => window.$wireui.notify({
+                        'title': 'Profile name saved',
+                        'description': `Good by, ${name}`,
+                        'icon': 'success'
+
+                    }) --}}
+                },
+                reject: {
+                    label: 'No, cancel'
+                }
+            })">
+        Refund
+    </x-button>
 </div>
