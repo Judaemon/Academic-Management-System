6ba5c8d4556837f78806e0ff672bbfa899c6a03a
John Ray Judaya <judaya93@gmail.com>
2022-12-07 09:45:00 +0800

    UI revision

--- resources/views/livewire/academic-year/actions-col.blade.php
+++ resources/views/livewire/academic-year/actions-col.blade.php
@@ -1,19 +1,17 @@
 <div class="flex flex-row space-x-2">
     @can('update_academic_years')
-        <x-button wire:ignore.self icon="pencil" info label="Edit"  
-            onclick="livewire.emit('openModal', 'academic-year.edit-academic-year', {{ json_encode(['academic_year' => $value]) }})" 
-        />
+        <x-button wire:ignore.self icon="pencil" info label="Edit"
+            onclick="livewire.emit('openModal', 'academic-year.edit-academic-year', {{ json_encode(['academic_year' => $value]) }})" />
     @endcan
 
     @can('read_academic_years')
-        <x-button wire:ignore.self icon="eye" green label="View"  
-            onclick="livewire.emit('openModal', 'academic-year.view-academic-year', {{ json_encode(['academic_year' => $value]) }})" 
-        />
+        <x-button wire:ignore.self icon="eye" green label="View"
+            onclick="livewire.emit('openModal', 'academic-year.view-academic-year', {{ json_encode(['academic_year' => $value]) }})" />
     @endcan
 
-    @can('delete_academic_years')
+    {{-- @can('delete_academic_years')
         <x-button wire:ignore.self icon="trash" negative label="Delete"
-            onclick="livewire.emit('openModal', 'academic-year.delete-academic-year', {{ json_encode(['academic_year' => $value]) }})" 
+            onclick="livewire.emit('openModal', 'academic-year.delete-academic-year', {{ json_encode(['academic_year' => $value]) }})"
         />
-    @endcan
+    @endcan --}}
 </div>
